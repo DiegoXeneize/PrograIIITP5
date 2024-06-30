4f590aee542d5885116dbd23ba3513b3143c0935
Matias-Bussetti <desarrollos.informaticos.bsstt@gmail.com>
2024-06-30 16:48:21 -0300

    tp terminado

--- Solver.java
+++ Solver.java
@@ -2,16 +2,36 @@ import ExpressionTree.Notation;
 import ExpressionTree.Node;
 import ExpressionTree.ExpresionTree;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Solver {
     public static double solve(String infixOperation) {
         String posfixNotation = Notation.infixToPostfix(infixOperation);
 
         Node tree = ExpresionTree.postfixToExpressionTree(posfixNotation);
 
-        ExpresionTree.toJson(tree);
+        saveStringToFile(ExpresionTree.toJson(tree));
 
         return ExpresionTree.solve(tree);
 
+    }
 
+    public static void saveStringToFile(String content) {
+        // Obtener el directorio actual
+        String currentDir = System.getProperty("user.dir");
+
+        // Crear un objeto File para tree.json en el directorio actual
+        File file = new File(currentDir, "ExpressionTree/tree.json");
+
+        // Utilizar FileWriter para escribir en el archivo, sobrescribiendo su contenido
+        // si ya existe
+        try (FileWriter fileWriter = new FileWriter(file)) {
+            fileWriter.write(content);
+            System.out.println("El contenido se ha guardado correctamente en tree.json");
+        } catch (IOException e) {
+            System.err.println("Ocurrió un error al guardar el archivo: " + e.getMessage());
+        }
     }
 }
